e551e2899797e27fd6056dfd20e528d0b06b0ea9
Rohit Jha <rohitj.ug19.cs@nitp.ac.in>
2022-04-17 16:55:24 +0530

    Added functionality of file storage on ipfs and fetching it.

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import NITP from '../NITP.png';
 import './App.css';
 
-
-// const ipfsClient = require('ipfs-http-client')
-// const ipfs = ipfsClient({host: "ipfs.infura.io", port:5001,  protocol: 'https'})
+//For file upload using ipfs
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({host: "ipfs.infura.io", port:5001,  protocol: 'https'})
 
 class App extends Component {
  constructor(props) {
    super(props);
    this.state = {
-     buffer: null
+     buffer: null,
+     fileHash: 'QmQMjGjgLdDMYTWdr1uyLeXxxY7xzRiG6zvyDQCWPeKwEJ'
    };
  }
 
@@ -35,10 +36,29 @@ class App extends Component {
   }
 
 
+  //hash = "QmQMjGjgLdDMYTWdr1uyLeXxxY7xzRiG6zvyDQCWPeKwEJ"
+  //url: https://ipfs.infura.io/ipfs/QmQMjGjgLdDMYTWdr1uyLeXxxY7xzRiG6zvyDQCWPeKwEJ
  //callback function for onSubmit event handler
  onSubmit = (event) => {
     event.preventDefault()
     console.log('submiting file')
+
+    //upload file to ipfs
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('ipfs result', result)
+
+      //getting hash of file from ipfs
+      const fileHash = result[0].hash
+      this.setState({fileHash: fileHash})
+      if(error){
+        console.error(error)
+        return
+      }
+
+      //store hash on blockchain
+    })
+
+    
  }
 
   render() {
@@ -76,6 +96,7 @@ class App extends Component {
                     <input type="file" onChange={this.captureFile}/>
                     <input type="submit" />
                   </form>
+                  <a className='fileshow'  href={`https://ipfs.infura.io/ipfs/${this.state.fileHash}`}>Get file</a>
                  </div>
               </div>
             </main>
